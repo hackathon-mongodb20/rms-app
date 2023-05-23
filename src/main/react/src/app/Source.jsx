178da2cf5f8c12c53053ecf0d3ec5be2bb3cf9b9
Matteo Franci <mttfranci@sogei.it>
2023-05-23 15:25:24 +0200

    Git repo

--- src/main/react/src/app/Source.jsx
+++ src/main/react/src/app/Source.jsx
@@ -5,7 +5,7 @@ class Source extends Component {
 	render() {
 		return <div style={{ margin: '20px' }}>
 			<p>Questa &egrave; solo una applicazione dimostrativa, trovate il codice sorgente al seguente link : 
-			<br/><a href="https://github.com/mttfranci/springboot-photobook">https://github.com/mttfranci/springboot-photobook</a></p>
+			<br/><a href="https://github.com/hackathon-mongodb20/rms-app">https://github.com/hackathon-mongodb20/rms-app</a></p>
 		</div>
 	}
 
