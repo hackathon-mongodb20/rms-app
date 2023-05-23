955d56654a35fa2350c09eadb9bb0b015b6f425e
Matteo Franci <mttfranci@sogei.it>
2023-05-23 13:26:00 +0200

    Initial import

---
+++ src/main/java/io/rms/app/RmsAppApplication.java
@@ -0,0 +1,13 @@
+package io.rms.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsAppApplication.class, args);
+	}
+
+}
